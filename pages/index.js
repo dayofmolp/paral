9b3c8f285176b11983fe464da084c503ea1e7b17
dayofmolp <molpofdays@gmail.com>
2024-11-18 12:35:38 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Paral</h1>
+      <p>This is a basic Next.js app for testing deployment on Vercel.</p>
+    </div>
+  );
+}
