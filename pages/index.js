241d94f3d9456ad52a209c8b94fa8e334609bd28
dayofmolp <molpofdays@gmail.com>
2024-11-18 12:45:49 +0200

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Welcome to Paral</h1>
-      <p>This is a basic Next.js app for testing deployment on Vercel.</p>
-    </div>
-  );
-}
